41f9b87a0a865b802fa6d0080cdbbd4f66d23eed
GPlay97 <GPlay97@users.noreply.github.com>
2019-11-23 19:24:37 +0100

    add missing export

---
+++ index.js
@@ -0,0 +1,7 @@
+const authorizationHandler = require('./modules/authorizationHandler');
+const errorHandler = require('./modules/errorHandler');
+
+module.exports = {
+    authorizationHandler,
+    errorHandler
+};
